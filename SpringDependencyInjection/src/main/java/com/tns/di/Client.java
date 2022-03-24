ae11e6f1c8dde509b2bb8ce6ad73c4ed9482b14f
Swamynathan-sam <swamynathansam7@gmail.com>
2022-03-24 20:49:29 +0530

    First Commit

---
+++ SpringDependencyInjection/src/main/java/com/tns/di/Client.java
@@ -0,0 +1,15 @@
+package com.tns.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client 
+{
+
+	public static void main(String[] args) 
+	{
+		ApplicationContext c=new ClassPathXmlApplicationContext("beans.xml");
+		c.
+	}
+
+}
---
+++ SpringDependencyInjection/src/main/java/com/tns/di1/Client.java
@@ -0,0 +1,10 @@
+package com.tns.di1;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Student s1=new Student();
+		s1.cheating();
+	}
+
+}
